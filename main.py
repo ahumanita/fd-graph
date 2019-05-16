45c4d27f03ca7ed20f59caf51df4cc89db91aa32
ahumanita <anita.ullrich@arcor.de>
2019-05-16 22:07:14 +0200

    added edge drawing

--- main.py
+++ main.py
@@ -10,19 +10,31 @@ def random_coords(V) :
 		coords.append([t_w,t_h])
 	return coords
 
-def draw_arcs(w,coords,arc_rad,width,height) :
+# place nodes according to the coordinates as ovals in canvas
+def draw_nodes(w,coords,arc_rad,width,height) :
 	for item in coords :
-		w.create_arc(int(item[0]*width+arc_rad)
+		w.create_oval(int(item[0]*width+arc_rad)
 			,int(item[1]*height+arc_rad)
 			,int(item[0]*width-arc_rad)
 			,int(item[1]*height-arc_rad)
 			,fill="blue")
 
+def draw_edges(w,Adj,coords,width,height) :
+	for i in range(len(Adj)) :
+		for edge in Adj[i] :
+			w.create_line(int(coords[i][0]*width)
+				,coords[i][1]*width
+				,coords[edge][0]*width
+				,coords[edge][1]*width
+				)
+
+
+
 if __name__ == "__main__" :
 	master = Tk()
 
-	w_width = 400
-	w_height = 400
+	w_width = 500
+	w_height = 500
 	arc_rad = 5
 
 	w = Canvas(master, width=w_width, height=w_height)
@@ -32,7 +44,7 @@ if __name__ == "__main__" :
 	Adj = [[1,2],[0,2],[1,2]]
 
 	coords = random_coords(V)
-	print(coords)
-	draw_arcs(w,coords,arc_rad,w_width,w_height)
+	draw_nodes(w,coords,arc_rad,w_width,w_height)
+	draw_edges(w,Adj,coords,w_width,w_height)
 
 	mainloop()
