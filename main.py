e122279e6b65b1241b985d5957c31976d8fe3766
ahumanita <anita.ullrich@arcor.de>
2019-05-19 12:29:50 +0200

    applied changes in structure

--- main.py
+++ main.py
@@ -1,50 +1,72 @@
 from tkinter import *
 from random import random
+from graph import Node, Edge, Graph
+from time import sleep
 
 # assert random coordinates to nodes of graph between 0 and 1
-def random_coords(V) :
-	coords = []
-	for item in V :
+def random_coords(V,width,height) :
+	for node in V :
 		t_w = random()
 		t_h = random()
-		coords.append([t_w,t_h])
-	return coords
+		node.x = (t_w*width,t_h*height)
 
 # place nodes according to the coordinates as ovals in canvas
-def draw_nodes(w,coords,arc_rad,width,height) :
-	for item in coords :
-		w.create_oval(int(item[0]*width+arc_rad)
-			,int(item[1]*height+arc_rad)
-			,int(item[0]*width-arc_rad)
-			,int(item[1]*height-arc_rad)
+def draw_nodes(w,V,arc_rad,width,height) :
+	for node in V :
+		w.create_oval(int(node.x[0]+arc_rad)
+			,int(node.x[1]+arc_rad)
+			,int(node.x[0]-arc_rad)
+			,int(node.x[1]-arc_rad)
 			,fill="blue")
 
-def draw_edges(w,Adj,coords,width,height) :
-	for i in range(len(Adj)) :
-		for edge in Adj[i] :
-			w.create_line(int(coords[i][0]*width)
-				,coords[i][1]*width
-				,coords[edge][0]*width
-				,coords[edge][1]*width
-				)
+def draw_edges(w,V,E,width,height) :
+	for edge in E :
+		start = V[edge.start]
+		target = V[edge.target]
+		w.create_line(int(start.x[0])
+			,int(start.x[1])
+			,int(target.x[0])
+			,int(target.x[1])
+			,width=2.5
+			,smooth=1
+			,arrow="last")
 
 
 
 if __name__ == "__main__" :
 	master = Tk()
 
-	w_width = 500
-	w_height = 500
+	w_width = 1200
+	w_height = 720
 	arc_rad = 5
 
 	w = Canvas(master, width=w_width, height=w_height)
 	w.pack()
 
-	V = [0,1,2]
-	Adj = [[1,2],[0,2],[1,2]]
+	V = [Node(0,Adj=[1,2])
+		,Node(1,Adj=[0,2])
+		,Node(2,Adj=[0,1])]
 
-	coords = random_coords(V)
-	draw_nodes(w,coords,arc_rad,w_width,w_height)
-	draw_edges(w,Adj,coords,w_width,w_height)
+	E = [Edge(0,0,1), Edge(1,0,2), Edge(2,1,2)]
+		#, Edge(3,1,2), Edge(4,2,0), Edge(4,2,1)]
+
+	# init node coordinates
+	random_coords(V,w_width,w_height)
+
+	G = Graph(V,E)
+
+	# force direction
+	while True :
+		sleep(0.05)
+		
+		# update node coordinates
+		G.update(w_width,w_height)
+
+		# clear window
+		w.delete("all")
+		# draw final graph
+		draw_nodes(w,V,arc_rad,w_width,w_height)
+		draw_edges(w,V,E,w_width,w_height)
+		w.update()
 
 	mainloop()
