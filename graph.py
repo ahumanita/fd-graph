981da88eaadc23cf15cc7966636ab85f52c480ad
ahumanita <anita.ullrich@arcor.de>
2019-05-23 20:15:49 +0200

    added attraction to north: gravity to imaginary point north of start point; added rejection of start node from top and right if too close

--- graph.py
+++ graph.py
@@ -1,20 +1,20 @@
 from math import sqrt
 
 class Node :
-	def __init__(self,id,x=(0,0),Adj=[],v=(0,0),a=(0,0),m=1) :
+	def __init__(self,id,x=(0,0),v=(0,0),a=(0,0),m=1) :
 		self.x = x
 		self.v = v
 		self.a = a
 		self.m = m
 		self.id = id
-		self.Adj = Adj
 
 class Edge :
-	def __init__(self,id,start,target,feather=0.0001) :
+	def __init__(self,id,start,target,feather=0.0001,dir="") :
 		self.id = id
 		self.start = start
 		self.target = target
 		self.feather = feather
+		self.dir = dir
 
 class Graph :
 	def __init__(self,V,E) :
@@ -35,10 +35,32 @@ class Graph :
 			target.a = (target.a[0]-edge.feather*(target.x[0]-start.x[0])
 						,target.a[1]-edge.feather*(target.x[1]-start.x[1]))
 
+			# directions
+			gravity = 5000
+			if edge.dir == "N" :
+				node = self.V[edge.target]
+				start = self.V[edge.start]
+				dist = sqrt((node.x[0]-start.x[0])**2 + (node.x[1]-start.x[1])**2)
+				
+				if start.x[1] < 2*dist/3 :
+					# avoid that start node sticks to top
+					start.a = (start.a[0],start.a[1]-gravity*(start.a[1]-height)/dist**3)
+
+				if start.x[0] < 2*dist/3 :
+					# avoid that start node sticks to right
+					start.a = (start.a[0]-gravity*(start.a[0]-height)/dist**3,start.a[1])
+
+				if node.x[1] > 0 and node.x[0]>0 and node.x[0] != start.x[0]:
+					# gravity to north
+					imx = start.x[0]
+					imy = start.x[1]-dist
+					dist = sqrt((node.x[0]-imx)**2 + (node.x[1]-imx)**2)
+					node.a = (node.a[0]-gravity*(node.x[0]-imx)/dist**3,node.a[1]-gravity*(node.x[1]-imy)/dist**3)
+
 
 		for node in self.V :
 			# electric force update
-			elec = 1000
+			elec = 500
 			for mnode in self.V :
 				if mnode != node :
 					dist = sqrt((node.x[0]-mnode.x[0])**2 + (node.x[1]-mnode.x[1])**2)
