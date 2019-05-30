45e125118cb6571b10de2fa818198fb7f5e31fe1
ahumanita <anita.ullrich@arcor.de>
2019-05-31 00:33:06 +0200

    not done yet, but: get all nodes and edges from data base and draw them. we had to subtract 1 from the index assignments in graph.py as the indices start by 1 instead of 0

--- graph.py
+++ graph.py
@@ -9,7 +9,7 @@ class Node :
 		self.id = id
 
 class Edge :
-	def __init__(self,id,start,target,feather=0.001,dir="") :
+	def __init__(self,id,start,target,feather=0.005,dir="") :
 		self.id = id
 		self.start = start
 		self.target = target
@@ -28,17 +28,17 @@ class Graph :
 
 		for edge in self.E :
 			# feather update
-			start = self.V[edge.start]
-			target = self.V[edge.target]
+			start = self.V[edge.start-1]
+			target = self.V[edge.target-1]
 			start.a = (start.a[0]-edge.feather*(start.x[0]-target.x[0])
 						,start.a[1]-edge.feather*(start.x[1]-target.x[1]))
 			target.a = (target.a[0]-edge.feather*(target.x[0]-start.x[0])
 						,target.a[1]-edge.feather*(target.x[1]-start.x[1]))
 
 			# directions
-			gravity = 5000
-			node = self.V[edge.target]
-			start = self.V[edge.start]
+			gravity = 1000
+			node = self.V[edge.target-1]
+			start = self.V[edge.start-1]
 			dist = sqrt((node.x[0]-start.x[0])**2 + (node.x[1]-start.x[1])**2)
 			tol = 2*dist/3
 
@@ -70,7 +70,7 @@ class Graph :
 					distim = sqrt((node.x[0]-imx)**2 + (node.x[1]-imx)**2)
 					node.a = (node.a[0]-gravity*(node.x[0]-imx)/distim**3
 								,node.a[1]-gravity*(node.x[1]-imy)/distim**3)
-					
+
 		for node in self.V :
 			# electric force update
 			elec = 500
@@ -81,7 +81,7 @@ class Graph :
 								,node.a[1]+(node.x[1]-mnode.x[1])*elec/(dist**3))
 
 			# drag update
-			drag = 0.1
+			drag = 0.5
 			node.a = (node.a[0]-node.v[0]*drag,node.a[1]-node.v[1]*drag)
 
 			# velocity update
--- main.py
+++ main.py
@@ -2,6 +2,76 @@ from tkinter import *
 from random import random
 from graph import Node, Edge, Graph
 from time import sleep
+import records
+
+db = records.Database(open("connection.txt","r").read().split()[0])
+
+def get_number_of_areas() :
+    number_a = db.query("SELECT max(area.id) FROM area")[0]["max"]
+    return number_a
+    
+def get_number_of_paths() :
+    number_p = db.query("SELECT max(path.id) FROM path")[0]["max"]
+    return number_p
+
+#pl: place, num_i: number of items, it#?: item, cr: creatures
+def get_areas(V) :
+    number_a = get_number_of_areas()
+    i = 1
+    area_list = []
+    #collect information for tikz-field
+    while i <= number_a :
+        act_area = {}
+        place = db.query("""SELECT area.name, area.id, area.light, area.directions_complete, area.items, 
+        				area.treasure, area.information, area.creatures FROM area WHERE area.id = :id""",
+        				id=i)
+       	# TODO: kann man bestimmt in Schleife packen!
+
+        act_area["name"] = place[0]["name"]
+        act_area["id"] = place[0]["id"]       
+        area_list.append(act_area)
+        
+        V.append(Node(i))
+        i += 1
+    return area_list
+
+#p_id: path id, st: start, des: destination, num_d: number of directions, d#: direction, col: color
+#TODO keys anzeigen (ort)
+def get_paths(E) :
+	number_p = get_number_of_paths()
+	path_list = []
+
+	i = 1
+	while i <= number_p :
+		act_path = {}
+		color = ""
+		path = db.query("""SELECT path.id, path.start, path.destination, path.direction,
+			path.usable, path.information, path.waterpath, path.notes
+			FROM path
+			WHERE path.id = :p_id
+			AND path.destination IS NOT NULL"""
+			, p_id = i)
+		path.all()
+
+		#check if destination is null or not
+		if not path :
+			pass
+			# path = db.query("""SELECT  path.id, path.start, path.destination, path.direction,
+			# 	path.usable, path.information, path.waterpath, path.notes FROM path WHERE path.id = :p_id"""
+			# 	, p_id = i)
+			# act_path["destination"] = "NON%d" %(i)
+			# graph.node("NON%d" %(i), "???")
+		else :
+			act_path["destination"] = path[0]["destination"]
+
+			act_path["id"] = path[0]["id"]
+			act_path["start"] = path[0]["start"]
+			act_path["direction"] = path[0]["direction"]
+			path_list.append(act_path)
+			E.append(Edge(i,int(path[0]["start"]),int(path[0]["destination"]),dir=path[0]["direction"]))
+		i += 1
+	return path_list
+
 
 # assert random coordinates to nodes of graph between 0 and 1
 def random_coords(V,width,height) :
@@ -21,8 +91,8 @@ def draw_nodes(w,V,arc_rad,width,height) :
 
 def draw_edges(w,V,E,width,height) :
 	for edge in E :
-		start = V[edge.start]
-		target = V[edge.target]
+		start = V[edge.start-1]
+		target = V[edge.target-1]
 		w.create_line(int(start.x[0])
 			,int(start.x[1])
 			,int(target.x[0])
@@ -43,12 +113,26 @@ if __name__ == "__main__" :
 	w = Canvas(master, width=w_width, height=w_height)
 	w.pack()
 
-	V = [Node(0,Adj=[1,2])
-		,Node(1,Adj=[0,2])
-		,Node(2,Adj=[0,1])]
 
-	E = [Edge(0,0,1), Edge(1,0,2), Edge(2,1,2)]
-		#, Edge(3,1,2), Edge(4,2,0), Edge(4,2,1)]
+	# V must be sorted by id correctly
+	# V = [Node(0)
+	# 	,Node(1)
+	# 	,Node(2)
+	# 	,Node(3)
+	# 	,Node(4)
+	# 	,Node(5)
+	# 	,Node(6)]
+
+	# E = [Edge(0,0,1), Edge(1,0,2), Edge(2,1,2), Edge(3,0,3,dir="N"),Edge(4,4,0,dir="N"),Edge(5,1,5,dir="D"),Edge(6,5,6,dir="S")]
+	# 	#, Edge(3,1,2), Edge(4,2,0), Edge(4,2,1)]
+
+	V = []
+	E = []
+	get_areas(V)
+	get_paths(E)
+
+	print(len(V))
+	print(len(E))
 
 	# init node coordinates
 	random_coords(V,w_width,w_height)
@@ -57,7 +141,7 @@ if __name__ == "__main__" :
 
 	# force direction
 	while True :
-		sleep(0.05)
+		#sleep(0.01)
 		
 		# update node coordinates
 		G.update(w_width,w_height)
