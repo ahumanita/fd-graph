9cee1714b1268e34f5fc0ebd8b2d46a76ccfc132
ahumanita <anita.ullrich@arcor.de>
2019-06-20 15:46:21 +0200

    combination of center gravitation, drag and electric force works as desired

--- graph.py
+++ graph.py
@@ -9,11 +9,10 @@ class Node :
 		self.id = id
 
 class Edge :
-	def __init__(self,id,start,target,feather=0.005,dir="") :
+	def __init__(self,id,start,target,dir="") :
 		self.id = id
 		self.start = start
 		self.target = target
-		self.feather = feather
 		self.dir = dir
 
 class Graph :
@@ -21,67 +20,82 @@ class Graph :
 		self.V = V
 		self.E = E
 
+	def electric_force_update(self,force,node) :
+		for mnode in self.V :
+			if mnode != node :
+				dist = sqrt((node.x[0]-mnode.x[0])**2 + (node.x[1]-mnode.x[1])**2)
+				node.a = (node.a[0]+(node.x[0]-mnode.x[0])*force/(dist**3)
+							,node.a[1]+(node.x[1]-mnode.x[1])*force/(dist**3))
+
+	# (gx,gy) is the point node is gravitated by
+	@staticmethod
+	def gravity_to_update(gravity,node,attractor) :
+		# mass = 9000
+		# dist = sqrt((node.x[0]-attractor[0])**2 + (node.x[1]-attractor[1])**2)
+		# node.a = (node.a[0]-gravity*mass*(node.x[0]-attractor[0])/dist**2
+		# 			,node.a[1]-gravity*mass*(node.x[1]-attractor[1])/dist**2)
+		dist = sqrt((node.x[0]-attractor[0])**2 + (node.x[1]-attractor[1])**2)
+		node.a = (node.a[0]-(node.x[0]-attractor[0])*gravity/(dist**3)
+					,node.a[1]-(node.x[1]-attractor[1])*gravity/(dist**3))
+
+	def feather_update(feather,start,target) :
+		start.a = (start.a[0]-edge.feather*(start.x[0]-target.x[0])
+					,start.a[1]-edge.feather*(start.x[1]-target.x[1]))
+		target.a = (target.a[0]-edge.feather*(target.x[0]-start.x[0])
+					,target.a[1]-edge.feather*(target.x[1]-start.x[1]))
+
+	def edge_orientation_update(gravity,start,node,dir) :
+		dist = sqrt((node.x[0]-start.x[0])**2 + (node.x[1]-start.x[1])**2)
+		tol = 2*dist/3
+		dir_coeff = {"N": (0,-1), "U": (0,-1)
+					, "S": (0,1), "D": (0,1)
+					, "E": (-1,0), "W": (1,0)
+					, "NE": (-1,-1), "NW": (1,-1)
+					, "SE": (-1,1), "SW": (1,1)
+					, "OUT": (0,0)}
+		# if edge.dir == "N" or edge.dir == "U" :
+		if edge.dir in dir_coeff.keys() :
+			# # avoid that start node sticks to top
+			# if start.x[1] < tol :
+			# 	start.a = (start.a[0],start.a[1]-gravity*(start.a[1]-height)/dist**3)
+
+			# # avoid that start node sticks to right
+			# if abs(start.x[0]-width) < tol :
+			# 	start.a = (start.a[0]-gravity*(start.a[0]-width)/dist**3,start.a[1])
+
+			# # avoid that start node sticks to left
+			# if start.x[0] < tol :
+			# 	start.a = (start.a[0]-gravity*(start.a[0]-width)/dist**3,start.a[1])
+
+			# gravity to north
+			if node.x[1] > tol and node.x[0] != start.x[0] :
+				imx = start.x[0] + dir_coeff[edge.dir][0]*dist
+				imy = start.x[1] + dir_coeff[edge.dir][1]*dist
+				gravity_to_update(gravity,node,(imx,imy))
+
 	def update(self,width,height) :
+		feather = 0.005
+		elec = 500
+		gravity_center = 1
+		drag = 0.9
+		gravity_orient = 500
+
+		center = (int(width/2),int(height/2))
+
 		# reset acceleration
 		for node in self.V :
 			node.a = (0,0)
 
 		for edge in self.E :
-			# feather update
 			start = self.V[edge.start-1]
 			target = self.V[edge.target-1]
-			start.a = (start.a[0]-edge.feather*(start.x[0]-target.x[0])
-						,start.a[1]-edge.feather*(start.x[1]-target.x[1]))
-			target.a = (target.a[0]-edge.feather*(target.x[0]-start.x[0])
-						,target.a[1]-edge.feather*(target.x[1]-start.x[1]))
-
-			# directions
-			gravity = 1000
-			node = self.V[edge.target-1]
-			start = self.V[edge.start-1]
-			dist = sqrt((node.x[0]-start.x[0])**2 + (node.x[1]-start.x[1])**2)
-			tol = 2*dist/3
-
-			dir_coeff = {"N": (0,-1), "U": (0,-1)
-						, "S": (0,1), "D": (0,1)
-						, "E": (-1,0), "W": (1,0)
-						, "NE": (-1,-1), "NW": (1,-1)
-						, "SE": (-1,1), "SW": (1,1)
-						, "OUT": (0,0)}
-
-			# if edge.dir == "N" or edge.dir == "U" :
-			if edge.dir in dir_coeff.keys() :
-				# # avoid that start node sticks to top
-				# if start.x[1] < tol :
-				# 	start.a = (start.a[0],start.a[1]-gravity*(start.a[1]-height)/dist**3)
-
-				# # avoid that start node sticks to right
-				# if abs(start.x[0]-width) < tol :
-				# 	start.a = (start.a[0]-gravity*(start.a[0]-width)/dist**3,start.a[1])
-
-				# # avoid that start node sticks to left
-				# if start.x[0] < tol :
-				# 	start.a = (start.a[0]-gravity*(start.a[0]-width)/dist**3,start.a[1])
-
-				# gravity to north
-				if node.x[1] > tol and node.x[0] != start.x[0] :
-					imx = start.x[0] + dir_coeff[edge.dir][0]*dist
-					imy = start.x[1] + dir_coeff[edge.dir][1]*dist
-					distim = sqrt((node.x[0]-imx)**2 + (node.x[1]-imx)**2)
-					node.a = (node.a[0]-gravity*(node.x[0]-imx)/distim**3
-								,node.a[1]-gravity*(node.x[1]-imy)/distim**3)
+			#feather_update(feather,start,target)
+			#edge_orientation_update(gravity_orient,start,target,edge.dir)
 
 		for node in self.V :
-			# electric force update
-			elec = 500
-			for mnode in self.V :
-				if mnode != node :
-					dist = sqrt((node.x[0]-mnode.x[0])**2 + (node.x[1]-mnode.x[1])**2)
-					node.a = (node.a[0]+(node.x[0]-mnode.x[0])*elec/(dist**3)
-								,node.a[1]+(node.x[1]-mnode.x[1])*elec/(dist**3))
+			self.electric_force_update(elec,node)	
 
 			# drag update
-			drag = 0.5
 			node.a = (node.a[0]-node.v[0]*drag,node.a[1]-node.v[1]*drag)
 
 			# velocity update
@@ -90,7 +104,7 @@ class Graph :
 			# coord update
 			upd = (node.x[0]+node.v[0], node.x[1]+node.v[1])
 
-			# TODO: min ( -node.v[0],node.v[0])
+			# get away from boundary
 			if upd[0] >= width :
 				node.v = (min(-node.v[0],node.v[0]),node.v[1])
 			elif upd[0] <= 0 :
@@ -100,6 +114,8 @@ class Graph :
 			elif upd[1] <= 0 :
 				node.v = (node.v[0],max(-node.v[1],node.v[1]))
 
-			upd = (node.x[0]+node.v[0], node.x[1]+node.v[1])
+			# gravity update
+			dist_cent = sqrt((node.x[0]-center[0])**2 + (node.x[1]-center[1])**2)
+			upd = (node.x[0]+node.v[0]-(node.x[0]-center[0])/dist_cent, node.x[1]+node.v[1]-(node.x[1]-center[1])/dist_cent)
 
 			node.x = (upd[0], upd[1])
