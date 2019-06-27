762686469c801346e8e82c63711fcce03263da04
ahumanita <anita.ullrich@arcor.de>
2019-06-27 22:56:21 +0200

    added correct intersection detection and corresponding orientation update

--- graph.py
+++ graph.py
@@ -1,4 +1,4 @@
-from math import sqrt
+from math import sqrt, acos
 
 class Node :
 	def __init__(self,id,x=(0,0),v=(0,0),a=(0,0),m=1) :
@@ -7,6 +7,7 @@ class Node :
 		self.a = a
 		self.m = m
 		self.id = id
+		self.orient = (0,0)
 
 class Edge :
 	def __init__(self,id,start,target,dir="") :
@@ -16,9 +17,17 @@ class Edge :
 		self.dir = dir
 
 class Graph :
-	def __init__(self,V,E) :
+	def __init__(self,V,E,tolerance) :
 		self.V = V
 		self.E = E
+		self.tolerance = tolerance
+		# i somehow had a sign error, this fits, dunno why
+		self.dir_coeff = {"N": (0,1), "U": (0,1)
+					, "S": (0,-1), "D": (0,-1)
+					, "E": (-1,0), "W": (1,0)
+					, "NE": (-1,-1), "NW": (1,-1)
+					, "SE": (-1,1), "SW": (1,1)
+					, "OUT": (0,0)}
 
 	def electric_force_update(self,force,node) :
 		for mnode in self.V :
@@ -30,67 +39,95 @@ class Graph :
 	# (gx,gy) is the point node is gravitated by
 	@staticmethod
 	def gravity_to_update(gravity,node,attractor) :
-		# mass = 9000
-		# dist = sqrt((node.x[0]-attractor[0])**2 + (node.x[1]-attractor[1])**2)
-		# node.a = (node.a[0]-gravity*mass*(node.x[0]-attractor[0])/dist**2
-		# 			,node.a[1]-gravity*mass*(node.x[1]-attractor[1])/dist**2)
 		dist = sqrt((node.x[0]-attractor[0])**2 + (node.x[1]-attractor[1])**2)
-		node.a = (node.a[0]-(node.x[0]-attractor[0])*gravity/(dist**3)
-					,node.a[1]-(node.x[1]-attractor[1])*gravity/(dist**3))
+		node.a = (node.a[0]-gravity*(node.x[0]-attractor[0])/dist**2
+					,node.a[1]-gravity*(node.x[1]-attractor[1])/dist**2)
 
-	def feather_update(feather,start,target) :
-		start.a = (start.a[0]-edge.feather*(start.x[0]-target.x[0])
-					,start.a[1]-edge.feather*(start.x[1]-target.x[1]))
-		target.a = (target.a[0]-edge.feather*(target.x[0]-start.x[0])
-					,target.a[1]-edge.feather*(target.x[1]-start.x[1]))
+	def feather_update(self,feather,start,target) :
+		start.a = (start.a[0]-feather*(start.x[0]-target.x[0])
+					,start.a[1]-feather*(start.x[1]-target.x[1]))
+		target.a = (target.a[0]-feather*(target.x[0]-start.x[0])
+					,target.a[1]-feather*(target.x[1]-start.x[1]))
 
-	def edge_orientation_update(gravity,start,node,dir) :
+	def edge_orientation_update(self,gravity,start,node,dir) :
 		dist = sqrt((node.x[0]-start.x[0])**2 + (node.x[1]-start.x[1])**2)
 		tol = 2*dist/3
-		dir_coeff = {"N": (0,-1), "U": (0,-1)
-					, "S": (0,1), "D": (0,1)
-					, "E": (-1,0), "W": (1,0)
-					, "NE": (-1,-1), "NW": (1,-1)
-					, "SE": (-1,1), "SW": (1,1)
-					, "OUT": (0,0)}
 		# if edge.dir == "N" or edge.dir == "U" :
-		if edge.dir in dir_coeff.keys() :
-			# # avoid that start node sticks to top
-			# if start.x[1] < tol :
-			# 	start.a = (start.a[0],start.a[1]-gravity*(start.a[1]-height)/dist**3)
-
-			# # avoid that start node sticks to right
-			# if abs(start.x[0]-width) < tol :
-			# 	start.a = (start.a[0]-gravity*(start.a[0]-width)/dist**3,start.a[1])
-
-			# # avoid that start node sticks to left
-			# if start.x[0] < tol :
-			# 	start.a = (start.a[0]-gravity*(start.a[0]-width)/dist**3,start.a[1])
-
+		if dir in self.dir_coeff.keys() :
+			update = (0,0)
 			# gravity to north
 			if node.x[1] > tol and node.x[0] != start.x[0] :
-				imx = start.x[0] + dir_coeff[edge.dir][0]*dist
-				imy = start.x[1] + dir_coeff[edge.dir][1]*dist
-				gravity_to_update(gravity,node,(imx,imy))
+				imx = start.x[0] + self.dir_coeff[dir][0]*dist
+				imy = start.x[1] + self.dir_coeff[dir][1]*dist
+				#Graph.gravity_to_update(gravity,node,(imx,imy))
+				dist = sqrt((node.x[0]-imx)**2 + (node.x[1]-imy)**2)
+				update = ((node.x[0]-imx)/dist,(node.x[1]-imy)/dist)
 
-	def update(self,width,height) :
+			# add orientation again in every step, but not in velocity!
+			node.orient = (node.orient[0]-update[0],node.orient[1]-update[1])
+
+	def nonoverlapping_edges_update(self,edge,start,target) :
+		for edge2 in self.E :
+			if edge2 is edge :
+				break
+			start2 = self.V[edge2.start-1]
+			target2 = self.V[edge2.target-1]
+			# do they intersect?
+			# target.x[0]-start.x[0]	-target2.x[0]+start2.x[0]	*	s 	= -start.x[0] + start2.x[0]
+			# target.x[1]-start.x[1]	-target2.x[1]+start2.x[1]	*	t 	= -start.x[1] + start2.x[1]
+			det = (target.x[0]-start.x[0])*(start2.x[1]-target2.x[1]) - (target.x[1]-start.x[1])*(start2.x[0]-target2.x[0])
+			if det != 0 and edge.dir in self.dir_coeff.keys() :
+				coeff = (1/det*((start2.x[1]-target2.x[1])*(start2.x[0]-start.x[0])+(target2.x[0]-start2.x[0])*(start2.x[1]-start.x[1]))
+						,1/det*((start.x[1]-target.x[1])*(start2.x[0]-start.x[0])+(target.x[0]-start.x[0])*(start2.x[1]-start.x[1])))
+				if 0 < coeff[0] < 1 and 0 < coeff[1] < 1 :
+					print("INTERSECT")
+					dist = sqrt((start.x[0]-coeff[0])**2 + (start.x[1]-coeff[1])**2)
+					start.orient = (start.orient[0]+self.dir_coeff[edge.dir][0]*(start.x[0]-coeff[0])/dist,start.orient[1]+self.dir_coeff[edge.dir][1]*(start.x[1]-coeff[1])/dist)
+					start.orient = (target.orient[0]+self.dir_coeff[edge.dir][0]*(start.x[0]-coeff[0])/dist,target.orient[1]+self.dir_coeff[edge.dir][1]*(start.x[1]-coeff[1])/dist)
+
+				# # get nearest point on line
+				# rhs = - ((start2.x[0]-start.x[0])*(target2.x[0]-start2.x[0]) + (start2.x[1]-start.x[1])*(target2.x[0]-start2.x[0]))
+				# coeff = (target2.x[0]-start2.x[0])**2 + (target2.x[1]-start2.x[1])**2
+				# if coeff == 0 :
+				# 	continue
+
+				# param = rhs/coeff
+				# # perpendicular point
+				# perp = (start2.x[0]+param*(target2.x[0]-start2.x[0]),start2.x[1]+param*(target2.x[1]-start2.x[1]))
+				# # distance from start to perp
+				# dist = sqrt((start.x[0]-perp[0])**2 + (start.x[1]-perp[1])**2)
+				# # add force that adds dist to the coords of start and target
+				# if dist == 0 :
+				# 	continue
+				# #start.orient = (start.orient[0]+(start.x[0]-perp[0])/dist,start.orient[1]+(start.x[1]-perp[1])/dist)
+				# # target.orient = (target.orient[0]+(start.x[0]-perp[0])/dist,target.orient[1]+(start.x[1]-perp[1])/dist)
+				# const = 0.00005
+				# start.orient = (start.orient[0]+self.dir_coeff[edge.dir][0]*const*(start.x[0]-perp[0]),start.orient[1]+self.dir_coeff[edge.dir][1]*const*(start.x[1]-perp[1]))
+				# #target.orient = (target.orient[0]+self.dir_coeff[edge.dir][0]*const*(start.x[0]-perp[0]),target.orient[1]+self.dir_coeff[edge.dir][1]*const*(start.x[1]-perp[1]))
+
+
+
+	def update(self,width,height,step) :
 		feather = 0.005
 		elec = 500
 		gravity_center = 1
 		drag = 0.9
 		gravity_orient = 500
+		tolerance = True
 
 		center = (int(width/2),int(height/2))
 
-		# reset acceleration
+		# reset acceleration and orientation
 		for node in self.V :
 			node.a = (0,0)
+			node.orient = (0,0)
 
 		for edge in self.E :
 			start = self.V[edge.start-1]
 			target = self.V[edge.target-1]
-			#feather_update(feather,start,target)
-			#edge_orientation_update(gravity_orient,start,target,edge.dir)
+			self.feather_update(feather,start,target)
+			self.edge_orientation_update(gravity_orient,start,target,edge.dir)
+			self.nonoverlapping_edges_update(edge,start,target)
 
 		for node in self.V :
 			self.electric_force_update(elec,node)	
@@ -102,20 +139,32 @@ class Graph :
 			node.v = (node.a[0]+node.v[0],node.a[1]+node.v[1])
 
 			# coord update
-			upd = (node.x[0]+node.v[0], node.x[1]+node.v[1])
+			upd = (node.x[0] + node.v[0] -node.orient[0], node.x[1] + node.v[1] -node.orient[1])
 
 			# get away from boundary
 			if upd[0] >= width :
 				node.v = (min(-node.v[0],node.v[0]),node.v[1])
+				node.orient = (0,0)
 			elif upd[0] <= 0 :
 				node.v = (max(-node.v[0],node.v[0]),node.v[1])
+				node.orient = (0,0)
 			if upd[1] >= height :
 				node.v = (node.v[0],min(-node.v[1],node.v[1]))
+				node.orient = (0,0)
 			elif upd[1] <= 0 :
 				node.v = (node.v[0],max(-node.v[1],node.v[1]))
+				node.orient = (0,0)
 
-			# gravity update
+			# final update + gravity update
 			dist_cent = sqrt((node.x[0]-center[0])**2 + (node.x[1]-center[1])**2)
-			upd = (node.x[0]+node.v[0]-(node.x[0]-center[0])/dist_cent, node.x[1]+node.v[1]-(node.x[1]-center[1])/dist_cent)
+			upd = (node.v[0]-(node.x[0]-center[0])/dist_cent-node.orient[0] ,node.v[1]-(node.x[1]-center[1])/dist_cent -node.orient[1])
+			#upd = (node.v[0] -node.orient[0],node.v[1] -node.orient[1])
 
-			node.x = (upd[0], upd[1])
+			# check if change is large
+			#if sqrt(upd[0]**2 + upd[1]**2) > self.tolerance :
+			#	tolerance = True
+
+			node.x = (node.x[0]+upd[0], node.x[1]+upd[1])
+
+
+		return tolerance
